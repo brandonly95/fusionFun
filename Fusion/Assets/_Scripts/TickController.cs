cde2a309e0ad2d78896541c599511b43a8349d4a
Unknown <ebuckland18@punahou.edu>
2018-03-19 20:29:36 -1000

    More changes, added master tick script

---
+++ Fusion/Assets/_Scripts/TickController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TickController : MonoBehaviour {
+
+
+	public float TickTime = .2;
+
+	private float oldTime;
+
+
+	// Use this for initialization
+	void Start () {
+		oldTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Time.time - oldTime > .2) {
+		
+		
+			GameObject[] gos = (GameObject[])GameObject.FindObjectsOfType(typeof(GameObject));
+			foreach (GameObject go in gos) {
+				if (go && go.transform.parent == null) {
+					go.gameObject.BroadcastMessage("Tick");
+				}
+			}
+
+			oldTime = Time.time;
+		
+		}
+		
+	}
+
+	IEnumerator ticker () {
+	
+
+
+
+
+	
+	}
+}
