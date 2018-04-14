6bd14da6e9d04b22d985aef0f3976ec916262689
Unknown <ebuckland18@punahou.edu>
2018-04-13 14:04:49 -1000

    Added working food to map

---
+++ Fusion/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+
+	public GameObject snakeMouth;
+
+	private Vector3 offset;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+
+		offset = this.transform.position;
+
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector3 finalPos = snakeMouth.transform.position + offset;
+
+
+		this.transform.position = Vector3.Slerp(this.transform.position,finalPos,speed);
+
+	}
+}
